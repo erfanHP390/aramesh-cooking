73ece5174e27b5c86d523bf47528e94323415553
erfan hosseini <erfanhosseini768@yahoo.com>
2025-05-18 17:56:58 +0330

    create page about-us

---
+++ src/app/about/page.js
@@ -0,0 +1,28 @@
+import Breadcrumb from '@/components/modules/breadcrumb/Breadcrumb'
+import Footer from '@/components/modules/footer/Footer'
+import Navbar from '@/components/modules/navbar/Navbar'
+import AboutUs from '@/components/templates/aboutUs/aboutUs/AboutUs'
+import Services from '@/components/templates/aboutUs/services/Services'
+import Subscribe from '@/components/templates/aboutUs/subscribe/Subscribe'
+import Statistic from '@/components/templates/index/statistic/Statistic'
+import Team from '@/components/templates/index/team/Team'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <Navbar />
+      <main id="content">
+        <Breadcrumb />
+        <AboutUs />
+        <Services />
+        <Statistic />
+        <Team />
+        <Subscribe />
+      </main>
+      <Footer/>
+    </>
+  )
+}
+
+export default page
