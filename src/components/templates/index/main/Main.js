00ddd6dbf5284ea39a7ace86260e6fd0ae3e427e
erfan hosseini <erfanhosseini768@yahoo.com>
2025-05-18 11:04:32 +0330

    import tem-compo  profile

--- src/components/templates/index/main/Main.js
+++ src/components/templates/index/main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../banner/Banner'
 import Profile from '../profile/Profile'
+import Services from '../services/Services'
 
 function Main() {
   return (
     <>
       <Banner />
       <Profile />
+      <Services />
     </>
   )
 }
