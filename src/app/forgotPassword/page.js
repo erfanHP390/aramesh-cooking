47cc2009dfcdcabd70e526a301a7446f50c1cb66
erfan hosseini <erfanhosseini768@yahoo.com>
2025-05-18 18:51:32 +0330

    create page forgot-password

---
+++ src/app/forgotPassword/page.js
@@ -0,0 +1,20 @@
+import Breadcrumb from '@/components/modules/breadcrumb/Breadcrumb'
+import Footer from '@/components/modules/footer/Footer'
+import Navbar from '@/components/modules/navbar/Navbar'
+import ForgotPass from '@/components/templates/forgot/ForgotPass'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <Navbar />
+      <main id="content">
+        <Breadcrumb />
+        <ForgotPass />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default page
