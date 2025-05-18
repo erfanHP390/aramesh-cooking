62216dec9758aaab7a329f1153f4a95dfd03cdd9
erfan hosseini <erfanhosseini768@yahoo.com>
2025-05-18 19:19:57 +0330

    create page login

---
+++ src/app/login/page.js
@@ -0,0 +1,20 @@
+import Breadcrumb from '@/components/modules/breadcrumb/Breadcrumb'
+import Footer from '@/components/modules/footer/Footer'
+import Navbar from '@/components/modules/navbar/Navbar'
+import Login from '@/components/templates/login/Login'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+     <Navbar />
+     <main id="content">
+        <Breadcrumb />
+        <Login />
+     </main>
+     <Footer /> 
+    </>
+  )
+}
+
+export default page
