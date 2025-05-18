33a319ed5bdb51bbf64086f3afaf70bcf19f5a90
erfan hosseini <erfanhosseini768@yahoo.com>
2025-05-18 09:48:36 +0330

    import tem-compo banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/modules/navbar/Navbar";
+import Main from "@/components/templates/index/main/Main";
 
 
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Main />
     </>
   );
 }
