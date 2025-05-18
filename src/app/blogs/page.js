40fcd9ace16fe33f1570c7466e51ed6167a7da8c
erfan hosseini <erfanhosseini768@yahoo.com>
2025-05-18 21:08:34 +0330

    create page blogs

---
+++ src/app/blogs/page.js
@@ -0,0 +1,20 @@
+import Breadcrumb from '@/components/modules/breadcrumb/Breadcrumb'
+import Footer from '@/components/modules/footer/Footer'
+import Navbar from '@/components/modules/navbar/Navbar'
+import AllBlog from '@/components/templates/blogs/allblog/AllBlog'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <Navbar />
+      <main id="content">
+        <Breadcrumb />
+        <AllBlog />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default page
