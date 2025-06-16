9830511508958a63140407bf75e8f87da194d8c1
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-16 19:28:02 +0330

    get departments of subject - contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,14 +3,21 @@ import Footer from '@/components/modules/footer/Footer'
 import Navbar from '@/components/modules/navbar/Navbar'
 import Contact from '@/components/templates/contact/ContactUs/Contact'
 import React from 'react'
+import connectToDB from "@/configs/db";
+import DepartmentModel from "@/models/Department"
 
-function page() {
+async function page() {
+
+    connectToDB()
+    const departments = await DepartmentModel.find({}).sort({_id: -1})
+  
+
   return (
     <>
       <Navbar />
       <main id="content">
         <Breadcrumb title={"تماس با ما"} />
-        <Contact />
+        <Contact  departments={departments} />
       </main>
       <Footer />
     </>
