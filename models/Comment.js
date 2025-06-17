06e35252df68156603a41be194c05ae6877437fa
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-17 22:53:33 +0330

    create model comment for blogs

---
+++ models/Comment.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+require("./Blog")
+
+const schema = mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    isAccept: {
+        type: Boolean,
+        default: false
+    },
+    blogID: {
+        type: mongoose.Types.ObjectId,
+        ref: "Blog"
+    }
+
+
+}, { timestamps: true });
+
+const model = mongoose.models.Comment || mongoose.model("Comment", schema);
+export default model
