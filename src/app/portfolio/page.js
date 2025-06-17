f066bc827662b709f99ff8bc771a9536e7c60dda
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-17 17:32:33 +0330

    get data from database and send to component

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,20 +1,27 @@
-import Breadcrumb from '@/components/modules/breadcrumb/Breadcrumb'
-import Footer from '@/components/modules/footer/Footer'
-import Navbar from '@/components/modules/navbar/Navbar'
-import Gallery from '@/components/templates/portfolio/Gallery'
-import React from 'react'
+import Breadcrumb from "@/components/modules/breadcrumb/Breadcrumb";
+import Footer from "@/components/modules/footer/Footer";
+import Navbar from "@/components/modules/navbar/Navbar";
+import Gallery from "@/components/templates/portfolio/Gallery";
+import React from "react";
+import connectToDB from "@/configs/db";
+import ProductModel from "@/models/Product";
+import CategoryModel from "@/models/Category";
 
-function page() {
+async function page() {
+  connectToDB();
+  const products = await ProductModel.find({}).populate("category").lean()
+  const categories = await CategoryModel.find({}).lean()
+
   return (
     <>
       <Navbar />
-      <main id="content">\
-        <Breadcrumb />
-        <Gallery />
+      <main id="content" className="flex-grow-1">
+        <Breadcrumb title={"نمونه ها"} />
+        <Gallery products={JSON.parse(JSON.stringify(products))} categories={JSON.parse(JSON.stringify(categories))} />
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default page
+export default page;
