7344d29641b9eb6724e66754dfd27b1f0bda63e2
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-21 11:42:43 +0330

    get blogs from database

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -2,15 +2,22 @@ import Breadcrumb from '@/components/modules/breadcrumb/Breadcrumb'
 import Footer from '@/components/modules/footer/Footer'
 import Navbar from '@/components/modules/navbar/Navbar'
 import AllBlog from '@/components/templates/blogs/allblog/AllBlog'
+import connectToDB from '@/configs/db'
 import React from 'react'
+import BlogModel from "@/models/Blog"
+import CommentModel from "@/models/Comment"
 
-function page() {
+async function page() {
+
+  connectToDB()
+  const blogs = await BlogModel.find({}).populate("comments").lean()
+
   return (
     <>
       <Navbar />
       <main id="content">
         <Breadcrumb title={"مقالات"} />
-        <AllBlog />
+        <AllBlog blogs={JSON.parse(JSON.stringify(blogs))} />
       </main>
       <Footer />
     </>
