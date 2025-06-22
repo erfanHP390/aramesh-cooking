1e0077d7d051fe2e02399c222d2e96a4911ba142
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-22 23:57:05 +0330

    update model-comment

--- models/Comment.js
+++ models/Comment.js
@@ -1,35 +1,44 @@
-const mongoose = require("mongoose")
-require("./Blog")
+const mongoose = require("mongoose");
+require("./Blog");
 
-const schema = mongoose.Schema({
-    
+const schema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     age: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+    },
+    isAnswer: {
+      type: Boolean,
+      default: false,
     },
     isAccept: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     blogID: {
-        type: mongoose.Types.ObjectId,
-        ref: "Blog"
-    }
-
-
-}, { timestamps: true });
+      type: mongoose.Types.ObjectId,
+      ref: "Blog",
+    },
+    mainComment: {
+      type: mongoose.Types.ObjectId,
+      ref: "Comment",
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
 
 const model = mongoose.models.Comment || mongoose.model("Comment", schema);
-export default model
+export default model;
