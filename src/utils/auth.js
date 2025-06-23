99178ead9e4c3db2b7e44ce1b91ef3509f2d5407
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-23 23:00:30 +0330

    add functions for hash-verify password and generate/verify token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,43 @@
+import { hash, compare } from "bcryptjs";
+import { sign, verify } from "jsonwebtoken";
+
+const hashPassword = async (password) => {
+  const hashedPassword = hash(password, 12);
+  return hashedPassword;
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+};
+
+const generateAccessToken = (data) => {
+  const token = sign({ ...data }, process.env.PRIVATEKEY, {
+    expiresIn: "10h", 
+  });
+  return token;
+};
+
+const verifyToken = (token) => {
+  try {
+    const tokenPayload = verify(token, process.env.PRIVATEKEY);
+    return tokenPayload;
+  } catch (err) {
+    console.log("access token verify is err", err);
+    return false;
+  }
+};
+
+const generateRefreshToken = (data) => {
+  const token = sign({ ...data }, process.env.RefreshTokenSecretKey, {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
 const validatePhone = (phone) => {
-  const regexPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g;
+  const regexPhone =
+    /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g;
   return regexPhone.test(phone);
 };
 
@@ -9,7 +47,8 @@ const validateEmail = (email) => {
 };
 
 const validatePassword = (password) => {
-  const regexPass = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g;
+  const regexPass =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g;
   return regexPass.test(password);
 };
 
@@ -17,4 +56,9 @@ export {
   validatePhone,
   validateEmail,
   validatePassword,
+  hashPassword,
+  verifyPassword,
+  generateAccessToken,
+  verifyToken,
+  generateRefreshToken,
 };
