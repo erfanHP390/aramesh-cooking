5bdfcac88cffc42b3c9708110e54796fe3ebde64
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-24 10:31:38 +0330

    create route with method api-post for refresh user-infos

---
+++ src/app/api/auth/refresh/route.js
@@ -0,0 +1,33 @@
+import connectToDB from "@/configs/db";
+import { cookies } from "next/headers";
+import UserModel from "@/models/User";
+import { verify } from "jsonwebtoken";
+import { generateAccessToken, generateRefreshToken } from "@/utils/auth";
+
+export async function POST(req) {
+    try {
+    connectToDB();
+    const refreshToken = cookies().get("refresh-token").value;
+
+    if (!refreshToken) {
+      return Response.json({ message: "user not authorized" }, { status: 401 });
+    }
+
+    const user = await UserModel.findOne({ refreshToken });
+
+    if (!user) {
+      return Response.json({ message: "not found" }, { status: 404 });
+    }
+
+    verify(refreshToken, process.env.RefreshTokenSecretKey);
+
+    return Response.json({ user }, { status: 200 });
+
+
+  } catch (err) {
+    return Response.json(
+      { message: `server error: ${err.message}` },
+      { status: 500 }
+    );
+  }
+}
