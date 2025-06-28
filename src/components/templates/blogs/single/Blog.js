d109079ad8ed9b1fd9588f6ae43f896a3b84b6a9
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-29 00:03:15 +0330

    revives answer form DB

--- src/components/templates/blogs/single/Blog.js
+++ src/components/templates/blogs/single/Blog.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./Blog.module.css";
-import CommentForm from "../commentForm/CommentForm";
+import Comments from "../comments/Comments";
+import connectToDB from "@/configs/db";
+import CommentModel from "@/models/Comment";
 
-function Blog({ blog }) {
+async function Blog({ blog }) {
+  connectToDB();
+  const answerComments = await CommentModel.find({ isAnswer: true });
+
   return (
     <>
       <div id="post-content" className={`${styles.postContainer} section pb-5`}>
@@ -13,13 +18,11 @@ function Blog({ blog }) {
                 {/* Post content */}
                 <div className="pt-3 pb-3">
                   <p>
-                    <h1 className={styles.postHeader}>
-                        {blog.title}
-                    </h1>
-                    <h2 className={styles.postAbstract}>
-                      چکیده
-                    </h2>
-                    <span className={styles.postAbstract}>{blog.shortDesc}</span>
+                    <h1 className={styles.postHeader}>{blog.title}</h1>
+                    <h2 className={styles.postAbstract}>چکیده</h2>
+                    <span className={styles.postAbstract}>
+                      {blog.shortDesc}
+                    </span>
                   </p>
                   <figure className={`${styles.postImage} align-center`}>
                     <img
@@ -27,14 +30,16 @@ function Blog({ blog }) {
                       src={blog.img}
                       alt="Image description"
                     />
-                    <figcaption className={styles.postImageCaption}>{blog.title}</figcaption>
+                    <figcaption className={styles.postImageCaption}>
+                      {blog.title}
+                    </figcaption>
                   </figure>
-                  <p className={styles.postDescription}>
-                    {blog.description}
-                  </p>
+                  <p className={styles.postDescription}>{blog.description}</p>
                 </div>
                 {/* author */}
-                <div className={`${styles.authorSection} row justify-content-center py-3`}>
+                <div
+                  className={`${styles.authorSection} row justify-content-center py-3`}
+                >
                   <div className="col-4 col-sm-3 col-md-2">
                     <a href="#">
                       <img
@@ -50,115 +55,31 @@ function Blog({ blog }) {
                       <span className="fw-bold">{blog.author}</span>
                     </p>
                     <p className="mb-1">
-                      <a className={styles.authorLink} href="#">دیدن مقالات بیشتر از این نویسنده</a>
+                      <a className={styles.authorLink} href="#">
+                        دیدن مقالات بیشتر از این نویسنده
+                      </a>
                     </p>
                     {/*description*/}
-                    <p className={styles.postDate}>تاریخ ثبت : {new Date(blog.createdAt).toLocaleDateString("fa-IR")}</p>
+                    <p className={styles.postDate}>
+                      تاریخ ثبت :{" "}
+                      {new Date(blog.createdAt).toLocaleDateString("fa-IR")}
+                    </p>
                   </div>
                 </div>
                 {/* Comments */}
                 <div id="comments" className="pt-4">
                   {/*title*/}
-                  <h3 className={styles.commentsHeader}>5 دیدگاه</h3>
+                  <h3 className={styles.commentsHeader}>
+                    (
+                    {blog.comments.filter((comment) => comment.isAccept).length}
+                    ) دیدگاه
+                  </h3>
                   {/*comment list*/}
-                  <ol className={`${styles.commentList} comment-list`}>
-                    <li>
-                      <div className={`${styles.userComment} u-hover`}>
-                        <footer>
-                          <img
-                            className={`${styles.commentAvatar} avatar-md border img-fluid float-start me-3`}
-                            src="/images/profile-user.png"
-                            alt="avatar"
-                          />
-                          <div className={styles.titleComments}>
-                            <a className={styles.commentAuthor} href="#" target="_blank">
-                              مهران احمدی
-                            </a>
-                            <span className={`${styles.commentDate} text-muted dates small`}>
-                              <time dateTime="2020-10-27">21 آذر 1403</time>
-                            </span>
-                          </div>
-                        </footer>
-                        <div className={styles.commentText}>
-                          <p>من این محتوا را دوست داشتم</p>
-                        </div>
-                        <div className={styles.btnReply}>
-                          <a className={styles.replyLink} href="#comment-form">
-                            پاسخ
-                          </a>
-                        </div>
-                      </div>
-                      <ul className={`${styles.subComment} list-unstyled sub-comment`}>
-                        <li>
-                          <div className={`${styles.userComment} u-hover`}>
-                            <footer>
-                              <img
-                                className={`${styles.commentAvatar} avatar-md border img-fluid float-start me-3`}
-                                src="/images/profile-user.png"
-                                alt="avatar"
-                              />
-                              <div className={styles.titleComments}>
-                                <a className={styles.commentAuthor} href="#" target="_blank">
-                                  علی شریفی
-                                </a>
-                                <span className={`${styles.commentDate} text-muted dates small`}>
-                                  <time dateTime="2020-10-27">21 آذر 1403</time>
-                                </span>
-                              </div>
-                            </footer>
-                            <div className={styles.commentText}>
-                              <p>خیلی خوشحالم از بازخورد شما!</p>
-                            </div>
-                            <div className={styles.btnReply}>
-                              <a className={styles.replyLink} href="#comment-form">
-                                پاسخ
-                              </a>
-                            </div>
-                          </div>
-                          <ul className={`${styles.subComment} list-unstyled sub-comment`}>
-                            <li className="comment " id="comment-5">
-                              <div className={`${styles.userComment} u-hover`}>
-                                <footer>
-                                  <img
-                                    className={`${styles.commentAvatar} avatar-md border img-fluid float-start me-3`}
-                                    src="/images/profile-user.png"
-                                    alt="avatar"
-                                  />
-                                  <div className={styles.titleComments}>
-                                    <a
-                                      className={styles.commentAuthor}
-                                      href="#"
-                                      target="_blank"
-                                    >
-                                      مهران احمدی
-                                    </a>
-                                    <span className={`${styles.commentDate} text-muted dates small`}>
-                                      <time dateTime="2020-10-27">
-                                        21 آذر 1403
-                                      </time>
-                                    </span>
-                                  </div>
-                                </footer>
-                                <div className={styles.commentText}>
-                                  <p>منم همین طور!</p>
-                                </div>
-                                <div className={styles.btnReply}>
-                                  <a
-                                    className={styles.replyLink}
-                                    href="#comment-form"
-                                  >
-                                    پاسخ
-                                  </a>
-                                </div>
-                              </div>
-                            </li>
-                          </ul>
-                        </li>
-                      </ul>
-                    </li>
-                  </ol>
-                  {/*comment form*/}
-                  <CommentForm  blogID={blog._id} />
+                  <Comments
+                    comments={blog.comments}
+                    blogID={blog._id}
+                    answers={answerComments}
+                  />
                 </div>
               </div>
             </div>
