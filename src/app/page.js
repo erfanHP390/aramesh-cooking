ec47e69e707b87d199d96ef89646a50fc6de719a
erfan hosseini <erfanhosseini768@yahoo.com>
2025-06-29 23:52:03 +0330

    valid user logged-in

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Footer from "@/components/modules/footer/Footer";
 import Navbar from "@/components/modules/navbar/Navbar";
 import Main from "@/components/templates/index/main/Main";
 import connectToDB from "@/configs/db";
+import { authUser } from "@/utils/authUser";
 import { cookies } from "next/headers";
 
 
@@ -9,10 +10,11 @@ export default async  function Home() {
 
   connectToDB()
   const isCookies = cookies().get("token")
+  const user = await authUser()
 
   return (
     <>
-    <Navbar isCookies={isCookies ? true : false} />
+    <Navbar isCookies={isCookies ? true : false} user={user} />
     <Main />
     <Footer />
     </>
