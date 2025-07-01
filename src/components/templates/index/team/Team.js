0bc3966b64a5dbe22867bb4f46d5ad66570a9148
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-01 22:43:12 +0330

    remove array team-members

--- src/components/templates/index/team/Team.js
+++ src/components/templates/index/team/Team.js
@@ -1,37 +1,8 @@
 import React from "react";
 import styles from "./Team.module.css";
+import { teamMembers } from "@/utils/constants";
 
 function Team() {
-  const teamMembers = [
-    {
-      id: 1,
-      name: "شیرین احمدی",
-      role: "سرآشپز غذاهای ایرانی",
-      image: "https://images.unsplash.com/photo-1601315488950-3b5047998b38?w=500&auto=format&fit=crop&q=60",
-      specialties: ["قورمه سبزی", "فسنجان", "چلوکباب"]
-    },
-    {
-      id: 2,
-      name: "رضا محمدی",
-      role: "متخصص پیش غذاها",
-      image: "https://images.unsplash.com/photo-1581299894007-aaa50297cf16?w=500&auto=format&fit=crop&q=60",
-      specialties: ["میرزا قاسمی", "کشک بادمجان", "آش رشته"]
-    },
-    {
-      id: 3,
-      name: "نیما رضایی",
-      role: "متخصص دسرها",
-      image: "https://images.unsplash.com/photo-1577219491135-ce391730fb2c?w=500&auto=format&fit=crop&q=60",
-      specialties: ["شیرینی پزی", "دسرهای سنتی", "بستنی خانگی"]
-    },
-    {
-      id: 4,
-      name: "مریم حسینی",
-      role: "سرآشپز بین المللی",
-      image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&auto=format&fit=crop&q=60",
-      specialties: ["غذاهای ایتالیایی", "سوشی", "استیک"]
-    }
-  ];
 
   return (
     <div id="team" className={`section pt-6 pt-md-7 pb-4 pb-md-5 ${styles.teamSection}`}>
